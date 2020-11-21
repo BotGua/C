2c3e45e31956d6504006040f8fd5cfcddb1ba566
BotGua <74773543+BotGua@users.noreply.github.com>
2020-11-21 22:20:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const menu = require("./lib/menu.js");
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'ALF BOT'; 
-const instagram = 'https://instagram.com/aditiaalfians'; 
-const whatsapp = 'wa.me/6285799496179'; 
+const BotName = 'Akb4r™'; 
+const instagram = 'https://instagram.com/kamaadoo_tanjiroo'; 
+const whatsapp = 'wa.me/6281368965962'; 
 const kapanbotaktif = '24 JAM'; 
-//const grupch1 = 'belum ada grup'; 
+const grupch1 = 'https://chat.whatsapp.com/K4vJLX34mvqIDS7cd05Gfi'; 
 //const grupch2 = 'belum ada grup' ; 
 const
 {
