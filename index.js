ab0eab7ce7700825277c3581a287df50d401535a
BotGua <74773543+BotGua@users.noreply.github.com>
2020-11-29 08:51:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -621,7 +621,34 @@ if (text.includes("!loli"))
     });
     }
 
-
+if (text.includes("!pokemon"))
+   {
+    var items = ["anime pokemon"];
+    var nime = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.fdci.se/rep.php?gambar=" + nime;
+    
+    axios.get(url)
+      .then((result) => {
+        var n = JSON.parse(JSON.stringify(result.data));
+        var nimek =  n[Math.floor(Math.random() * n.length)];
+        imageToBase64(nimek) 
+        .then(
+            (response) => {
+	var buf = Buffer.from(response, 'base64'); 
+              conn.sendMessage(
+            id,
+              buf,MessageType.image)
+       
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error);
+            }
+        )
+    
+    });
+    }
 
 
 
