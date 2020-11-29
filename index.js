28dee58ed32c72072118693280ba3ca05f027d56
BotGua <74773543+BotGua@users.noreply.github.com>
2020-11-29 08:48:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -592,7 +592,34 @@ if (text.includes("!hentai"))
     });
     } 
 
-
+if (text.includes("!loli"))
+   {
+    var items = ["anime loli","anime loli sange","anime loli fackgirll","anime loli i love you"];
+    var nime = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.fdci.se/rep.php?gambar=" + nime;
+    
+    axios.get(url)
+      .then((result) => {
+        var n = JSON.parse(JSON.stringify(result.data));
+        var nimek =  n[Math.floor(Math.random() * n.length)];
+        imageToBase64(nimek) 
+        .then(
+            (response) => {
+	var buf = Buffer.from(response, 'base64'); 
+              conn.sendMessage(
+            id,
+              buf,MessageType.image)
+       
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error);
+            }
+        )
+    
+    });
+    }
 
 
 
